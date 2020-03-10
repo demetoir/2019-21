46858c995a895523c14552d5b0c095f4fea4c53c
demetoir-destop <wnsqlehlswk@naver.com>
2020-03-11 00:14:09 +0900

    enhance: resolve todo: ./config의 *.config.js 에서 로드 하도록 변경
    * backend/DB/config/..의 사용되지 않는 config를 실제로 사용되는 backend/DB/sequelize.config.js 에서 로드하도록 변경
    
    * 분할된 config에서 .env를 로드하도록 변경

--- backend/DB/config/mySQL.prod.config.js
+++ backend/DB/config/mySQL.prod.config.js
@@ -1,12 +1,17 @@
-require("dotenv").config();
+const path = require("path");
+
+const envPath = path.resolve(__dirname, "../../.env");
+
+require("dotenv").config({path: envPath});
 
 const host = process.env.MYSQL_PROD_HOST;
-const user = process.env.MYSQL_PROD_USER;
+const username = process.env.MYSQL_PROD_USER;
 const password = process.env.MYSQL_PROD_PASSWORD;
 const scheme = process.env.MYSQL_PROD_SCHEME;
 const dialect = process.env.MYSQL_PROD_DIALECT;
 const port = process.env.MYSQL_PROD_PORT;
 const containerName = process.env.MYSQL_PROD_CONTAINER_NAME;
+const logging = false;
 const pool = {
 	max: 5,
 	min: 0,
@@ -14,13 +19,17 @@ const pool = {
 	idle: 10000,
 };
 
-export const config = {
+const config = {
 	host,
-	user,
+	username,
 	password,
 	scheme,
 	dialect,
 	port,
 	pool,
 	containerName,
+	logging,
+	database: scheme,
 };
+
+export default config;
