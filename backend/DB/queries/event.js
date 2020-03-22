21fbe62c6ec1d94406f2d734935fd5606f8f00e5
demetoir-destop <wnsqlehlswk@naver.com>
2020-03-22 19:23:12 +0900

    chore: add todo: rename to findOrCreateEvent

--- backend/DB/queries/event.js
+++ backend/DB/queries/event.js
@@ -21,6 +21,7 @@ export async function getAllEvents() {
  * @param endAt {Date}
  * @returns {Promise<object>}
  */
+// todo: rename to findOrCreateEvent
 export async function createEvent({
 	eventName,
 	eventCode,
