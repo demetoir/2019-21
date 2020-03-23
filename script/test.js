a45f639d02eeec06eb4ca8d4845a839e351e3a4f
demetoir-destop <wnsqlehlswk@naver.com>
2020-03-23 17:29:32 +0900

    fix: test command of travis ci
    change bash script to javascript

---
+++ script/test.js
@@ -0,0 +1,41 @@
+const shell = require("shelljs");
+
+let code;
+// backend test
+shell.echo("test backend");
+shell.cd("./backend");
+code = shell.exec("yarn test");
+if (code !== 0) {
+  shell.exit();
+}
+shell.cd("..");
+
+// test front guest-app
+shell.echo("test front guest-app");
+shell.cd("./frontend/guest-app");
+code = shell.exec("yarn test a --watchAll=false");
+if (code !== 0) {
+  shell.exit();
+}
+shell.cd("..");
+shell.cd("..");
+
+// test front host-app
+shell.echo("test front host-app");
+shell.cd("./frontend/host-app");
+code = shell.exec("yarn test a --watchAll=false");
+if (code !== 0) {
+  shell.exit();
+}
+shell.cd("..");
+shell.cd("..");
+
+// test front main-app
+shell.echo("test front main-app");
+shell.cd("./frontend/main-app");
+code = shell.exec("yarn test a --watchAll=false");
+if (code !== 0) {
+  shell.exit();
+}
+shell.cd("..");
+shell.cd("..");
