11774f5ca8b1132e0c2744045f307d6732409a3e
demetoir-destop <wnsqlehlswk@naver.com>
2020-03-23 18:04:02 +0900

    fix: test script command of travis ci
    원인: exit code integer 값을 반환해야하는데 exit 정보 전체를 넘겨줌

--- script/test.js
+++ script/test.js
@@ -5,17 +5,18 @@ let code;
 shell.echo("test backend");
 shell.cd("./backend");
 shell.exec("cp .env.example .env");
-code = shell.exec("yarn test");
+code = shell.exec("yarn test").code;
 if (code !== 0) {
-  // shell.exit(code);
-  process.exit(code);
+  shell.exit(code);
+  // console.log(code)
+  // process.exit(code);
 }
 shell.cd("..");
 
 // test front guest-app
 shell.echo("test front guest-app");
 shell.cd("./frontend/guest-app");
-code = shell.exec("yarn test a --watchAll=false");
+code = shell.exec("yarn test a --watchAll=false").code;
 if (code !== 0) {
   shell.exit(code);
 }
@@ -25,7 +26,7 @@ shell.cd("..");
 // test front host-app
 shell.echo("test front host-app");
 shell.cd("./frontend/host-app");
-code = shell.exec("yarn test a --watchAll=false");
+code = shell.exec("yarn test a --watchAll=false").code;
 if (code !== 0) {
   shell.exit(code);
 }
@@ -35,7 +36,7 @@ shell.cd("..");
 // test front main-app
 shell.echo("test front main-app");
 shell.cd("./frontend/main-app");
-code = shell.exec("yarn test a --watchAll=false");
+code = shell.exec("yarn test a --watchAll=false").code;
 if (code !== 0) {
   shell.exit(code);
 }
