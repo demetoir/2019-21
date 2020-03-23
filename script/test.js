57c92cecce1b32c0a89b727b26468c1b7f07bf19
demetoir-destop <wnsqlehlswk@naver.com>
2020-03-23 18:33:12 +0900

    chore: test script를 back/front-end 별로  파일 분리

--- script/test.js
+++ script/test.js
@@ -1,42 +1,4 @@
-const shell = require("shelljs");
-
-let code;
-// backend test
-shell.echo("test backend");
-shell.cd("./backend");
-shell.exec("cp .env.example .env");
-code = shell.exec("yarn test").code;
-if (code !== 0) {
-  shell.exit(code);
-}
-shell.cd("..");
-
-// test front guest-app
-shell.echo("test front guest-app");
-shell.cd("./frontend/guest-app");
-code = shell.exec("yarn test a --watchAll=false").code;
-if (code !== 0) {
-  shell.exit(code);
-}
-shell.cd("..");
-shell.cd("..");
-
-// test front host-app
-shell.echo("test front host-app");
-shell.cd("./frontend/host-app");
-code = shell.exec("yarn test a --watchAll=false").code;
-if (code !== 0) {
-  shell.exit(code);
-}
-shell.cd("..");
-shell.cd("..");
-
-// test front main-app
-shell.echo("test front main-app");
-shell.cd("./frontend/main-app");
-code = shell.exec("yarn test a --watchAll=false").code;
-if (code !== 0) {
-  shell.exit(code);
-}
-shell.cd("..");
-shell.cd("..");
+require("./test.backend.js");
+require("./test.frontend.guest-app.js");
+require("./test.frontend.host-app.js");
+require("./test.frontend.main-app.js");
