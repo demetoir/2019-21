04f2e3678442a6bdfc5e6bdd136f73b26835acad
demetoir-destop <wnsqlehlswk@naver.com>
2020-03-23 14:34:23 +0900

    refactoring: change socket handler name
    why: socket handler의 이름 일관성 유지
    
    change function name: moveQuestionSocketHandler -> addEmojiSocketHandler
    change function name: questionCreateSocketHandler -> createQuestionSocketHandler
    change function name: helloHandler -> helloSocketHandler

--- backend/socket_io_server/socketHandler/emoji/addEmoji.socketHandler.js
+++ backend/socket_io_server/socketHandler/emoji/addEmoji.socketHandler.js
@@ -1,6 +1,6 @@
 import {createEmoji} from "../../../DB/queries/emoji.js";
 
-const moveQuestionSocketHandler = async (data, emit) => {
+const addEmojiSocketHandler = async (data, emit) => {
 	const {GuestId, name, EventId, QuestionId} = data;
 	const res = await createEmoji({GuestId, name, EventId, QuestionId});
 
@@ -11,5 +11,5 @@ const eventName = "questionEmoji/create";
 
 export default {
 	eventName,
-	handler: moveQuestionSocketHandler,
+	handler: addEmojiSocketHandler,
 };
