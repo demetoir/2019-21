3fa8b5b75a4c0b85ab7bb3f8ccff492cc1444c1f
demetoir-destop <wnsqlehlswk@naver.com>
2020-03-23 19:05:30 +0900

    chore: add install stage on travis ci

--- script/install.js
+++ script/install.js
@@ -1,27 +1,4 @@
-var shell = require("shelljs");
-
-shell.cd("./backend");
-shell.exec("yarn install");
-shell.cd("..");
-
-shell.cd("./frontend/guest-app");
-shell.exec("yarn install");
-shell.cd("..");
-shell.cd("..");
-
-shell.cd("./frontend/host-app");
-shell.exec("yarn install");
-shell.cd("..");
-shell.cd("..");
-
-shell.cd("./frontend/main-app");
-shell.exec("yarn install");
-shell.cd("..");
-shell.cd("..");
-
-//
-// // Run external tool synchronously
-// if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
-// 	shell.echo('Error: Git commit failed');
-// 	shell.exit(1);
-// }
+require("./install.frontend.guest-app.js");
+require("./install.frontend.host-app.js");
+require("./install.frontend.main-app.js");
+require("./install.backend.js");
