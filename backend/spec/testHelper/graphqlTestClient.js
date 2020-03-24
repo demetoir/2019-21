8fbd395bcb33a111b15e3bd0548d0f2b2e5c8a85
demetoir-destop <wnsqlehlswk@naver.com>
2020-03-24 23:03:24 +0900

    fix: 불필요한 test code 및 test helper 삭제

--- backend/spec/testHelper/graphqlTestClient.js
+++
@@ -1,6 +0,0 @@
-import {GraphQLClient} from "graphql-request";
-import config from "../../graphQL/config.js";
-
-const endpointUrl = `http://localhost:${config.port}${config.endpoint}`;
-
-export default new GraphQLClient(endpointUrl, {headers: {}});
